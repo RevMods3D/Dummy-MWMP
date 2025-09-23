5a2df483cc5ac68d0ba62ebd1cbdb0c41b75dcc9
RevMods3D <104259452+RevMods3D@users.noreply.github.com>
2025-09-23 14:22:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,27 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('.'));
 
 // Dummy data
-const playerNames = ["Alex","Blake","Casey","Drew","Jordan","Taylor","Sam","Riley", "James", "Jake", "Duke"];
-const serverModes = ["Race","FreeRoam","Chase", "Cop"];
-const serverTags = ["US","EU","ASIA", "China"];
+const playerNames = ["Avery","Cameron","Logan","Quinn","Morgan","Reese","Skyler","Peyton","Hayden","Charlie",
+"Bailey","Casey","Rowan","Dakota","Hunter","Jessie","Emerson","Finley","Blake","Jamie",
+"Alex","Taylor","Riley","Jordan","Sam","Drew","James","Jake","Duke","Micah",
+"Adrian","Cory","Shawn","Taylor","Kai","Elliot","Devon","Shane","Spencer","Logan",
+"Phoenix","Cameron","River","Rory","Toby","Blair","Sasha","Sky","Alexis","Remy",
+"Chris","Reagan","Marley","Parker","Jesse","Tyler","Hunter","Morgan","Casey","Kai",
+"Rowan","Emery","Dallas","Finley","Jaden","Justice","Quinn","Reese","Sawyer","Taylor"];
+const serverModes = ["Race","FreeRoam","Chase", "Cop", "HotPursuit", "Showcase", "Elimination", "Drift"];
+const serverTags = ["US","EU","ASIA", "China", "Casual", "Competitive", "Stunt", "NoPolice", "NoCollision", "Bonuses"];
 
 // Servers
 let servers = [
-    { name: "Server Alpha", ip: "127.0.0.1", maxPlayers: 30, mode: "", tag: "", ping: "", online: true, players: [] },
-    { name: "Server Beta", ip: "127.0.0.2", maxPlayers: 30, mode: "", tag: "", ping: "", online: true, players: [] }
+    { name: "Server Alpha", ip: "204.31.190.249", maxPlayers: 30, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Beta", ip: "181.159.46.35", maxPlayers: 60, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Gamma", ip: "142.34.189.239", maxPlayers: 5, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Eta", ip: "187.8.149.1", maxPlayers: 25, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Theta", ip: "133.22.63.96", maxPlayers: 65, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Iota ", ip: "140.232.55.101", maxPlayers: 55, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Nu", ip: "169.237.51.64", maxPlayers: 85, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Xi", ip: "41.67.110.24", maxPlayers: 12, mode: "", tag: "", ping: "", online: true, players: [] },
+    { name: "Server Omicron ", ip: "219.90.54.183", maxPlayers: 100, mode: "", tag: "", ping: "", online: true, players: [] },
 ];
 
 // Random player generator
@@ -22,7 +35,7 @@ function generatePlayers(count) {
         const name = playerNames[Math.floor(Math.random() * playerNames.length)];
         players.push({
             name: name,
-            pping: Math.floor(Math.random() * 100),   // <-- changed property name from ping to pping
+            pping: Math.floor(Math.random() * 100),
             time: Math.floor(Math.random() * 1200)
         });
     }
